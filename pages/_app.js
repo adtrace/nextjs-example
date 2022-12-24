0240903d77bb0872f9cb1d64681c93d6303830dd
mohammad <mohammadetedadi@gmail.com>
2022-12-24 16:13:25 +0330

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
+import { AdtraceWrapper } from '../utils/adtrace'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AdtraceWrapper>
+    <Component {...pageProps} />
+  </AdtraceWrapper>
 }
---
+++ utils/adtrace.js
@@ -0,0 +1,27 @@
+import { createContext, useContext } from 'react';
+
+const Adtrace = createContext();
+
+export function AdtraceWrapper({ children }) {
+  let AdtraceSdk
+
+  if (typeof window !== 'undefined') {
+    AdtraceSdk = require('web-adtrace')
+
+    AdtraceSdk.initSdk({
+      appToken: "YOUR_APP_TOKEN",
+      environment: "production", // or "sandbox"
+      // ... other optional parameters if needed
+    })
+  }
+
+  return (
+    <Adtrace.Provider value={AdtraceSdk}>
+      {children}
+    </Adtrace.Provider>
+  );
+}
+
+export function useAdtraceContext() {
+  return useContext(Adtrace);
+}
